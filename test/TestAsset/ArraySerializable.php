67edcffdc1d621934b0791efa3652d8d641d1f13
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:51:07 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/ArraySerializable.php
+++ test/TestAsset/ArraySerializable.php
@@ -1,18 +1,17 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
-namespace ZendTest\Stdlib\TestAsset;
 
 /**
- * @group      Zend_Stdlib
+ * @see       https://github.com/laminas/laminas-stdlib for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-stdlib/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-stdlib/blob/master/LICENSE.md New BSD License
  */
-class ArraySerializable implements \Zend\Stdlib\ArraySerializableInterface
+
+namespace LaminasTest\Stdlib\TestAsset;
+
+/**
+ * @group      Laminas_Stdlib
+ */
+class ArraySerializable implements \Laminas\Stdlib\ArraySerializableInterface
 {
     protected $data = array();
 
