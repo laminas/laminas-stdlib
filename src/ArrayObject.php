11d7c6a867b905258beffe1a1b422c0bfccde063
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:49:32 -0600

    Rewriting as Laminas Project package

--- src/ArrayObject.php
+++ src/ArrayObject.php
@@ -1,27 +1,26 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-stdlib for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-stdlib/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-stdlib/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Stdlib;
+namespace Laminas\Stdlib;
 
 /**
- * If the version is less than 5.3.4, we'll use Zend\Stdlib\ArrayObject\PhpLegacyCompatibility
+ * If the version is less than 5.3.4, we'll use Laminas\Stdlib\ArrayObject\PhpLegacyCompatibility
  * which extends the native PHP ArrayObject implementation. For versions greater than or equal
- * to 5.3.4, we'll use Zend\Stdlib\ArrayObject\PhpReferenceCompatibility, which corrects
+ * to 5.3.4, we'll use Laminas\Stdlib\ArrayObject\PhpReferenceCompatibility, which corrects
  * issues with how PHP handles references inside ArrayObject.
  *
- * class_alias is a global construct, so we can alias either one to Zend\Stdlib\ArrayObject,
+ * class_alias is a global construct, so we can alias either one to Laminas\Stdlib\ArrayObject,
  * and from this point forward, that alias will be used.
  */
 if (version_compare(PHP_VERSION, '5.3.4', 'lt')) {
-    class_alias('Zend\Stdlib\ArrayObject\PhpLegacyCompatibility', 'Zend\Stdlib\AbstractArrayObject');
+    class_alias('Laminas\Stdlib\ArrayObject\PhpLegacyCompatibility', 'Laminas\Stdlib\AbstractArrayObject');
 } else {
-    class_alias('Zend\Stdlib\ArrayObject\PhpReferenceCompatibility', 'Zend\Stdlib\AbstractArrayObject');
+    class_alias('Laminas\Stdlib\ArrayObject\PhpReferenceCompatibility', 'Laminas\Stdlib\AbstractArrayObject');
 }
 
 /**
