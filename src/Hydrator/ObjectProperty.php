812abfb59cf3aa10232dc2d742cd85dbc3d0d417
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:50:54 -0600

    Rewriting as Laminas Project package

--- src/Hydrator/ObjectProperty.php
+++ src/Hydrator/ObjectProperty.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-stdlib for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-stdlib/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-stdlib/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Stdlib\Hydrator;
+namespace Laminas\Stdlib\Hydrator;
 
-use Zend\Hydrator\ObjectProperty as BaseObjectProperty;
+use Laminas\Hydrator\ObjectProperty as BaseObjectProperty;
 
 /**
- * @deprecated Use Zend\Hydrator\ObjectProperty from zendframework/zend-hydrator instead.
+ * @deprecated Use Laminas\Hydrator\ObjectProperty from laminas/laminas-hydrator instead.
  */
 class ObjectProperty extends BaseObjectProperty implements HydratorInterface
 {
--- src/Hydrator/Strategy/Exception/ExceptionInterface.php
+++ src/Hydrator/Strategy/Exception/ExceptionInterface.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-stdlib for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-stdlib/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-stdlib/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Stdlib\Hydrator\Strategy\Exception;
+namespace Laminas\Stdlib\Hydrator\Strategy\Exception;
 
-use Zend\Hydrator\Strategy\Exception;
+use Laminas\Hydrator\Strategy\Exception;
 
 /**
- * @deprecated Use Zend\Hydrator\Strategy\Exception\ExceptionInterface from zendframework/zend-hydrator instead.
+ * @deprecated Use Laminas\Hydrator\Strategy\Exception\ExceptionInterface from laminas/laminas-hydrator instead.
  */
 interface ExceptionInterface extends Exception\ExceptionInterface
 {
