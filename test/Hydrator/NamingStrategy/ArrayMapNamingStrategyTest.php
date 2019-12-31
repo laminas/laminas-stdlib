faf5960bd08d44f527f2bc30ca260680f04568eb
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:50:26 -0600

    Rewriting as Laminas Project package

--- test/Hydrator/NamingStrategy/ArrayMapNamingStrategyTest.php
+++ test/Hydrator/NamingStrategy/ArrayMapNamingStrategyTest.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-stdlib for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-stdlib/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-stdlib/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Stdlib\Hydrator\NamingStrategy;
+namespace LaminasTest\Stdlib\Hydrator\NamingStrategy;
 
-use Zend\Stdlib\Hydrator\NamingStrategy\ArrayMapNamingStrategy;
+use Laminas\Stdlib\Hydrator\NamingStrategy\ArrayMapNamingStrategy;
 
 /**
- * Tests for {@see \Zend\Stdlib\Hydrator\NamingStrategy\ArrayMapNamingStrategy}
+ * Tests for {@see \Laminas\Stdlib\Hydrator\NamingStrategy\ArrayMapNamingStrategy}
  *
- * @covers \Zend\Stdlib\Hydrator\NamingStrategy\ArrayMapNamingStrategy
+ * @covers \Laminas\Stdlib\Hydrator\NamingStrategy\ArrayMapNamingStrategy
  */
 class ArrayMapNamingStrategyTest extends \PHPUnit_Framework_TestCase
 {
