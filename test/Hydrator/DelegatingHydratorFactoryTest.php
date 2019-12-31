dff071b242f5e8956a187b9d982a6d0d87a03154
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:50:33 -0600

    Rewriting as Laminas Project package

--- test/Hydrator/DelegatingHydratorFactoryTest.php
+++ test/Hydrator/DelegatingHydratorFactoryTest.php
@@ -1,24 +1,23 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-stdlib for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-stdlib/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-stdlib/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Stdlib\Hydrator;
+namespace LaminasTest\Stdlib\Hydrator;
 
-use Zend\Stdlib\Hydrator\DelegatingHydratorFactory;
+use Laminas\Stdlib\Hydrator\DelegatingHydratorFactory;
 
 class DelegatingHydratorFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testFactory()
     {
-        $hydratorManager = $this->getMock('Zend\ServiceManager\ServiceLocatorInterface');
+        $hydratorManager = $this->getMock('Laminas\ServiceManager\ServiceLocatorInterface');
         $factory = new DelegatingHydratorFactory();
         $this->assertInstanceOf(
-            'Zend\Stdlib\Hydrator\DelegatingHydrator',
+            'Laminas\Stdlib\Hydrator\DelegatingHydrator',
             $factory->createService($hydratorManager)
         );
     }
