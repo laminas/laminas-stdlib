9363c2f0a5fc50b1de0975f0cc40c7b79c5cb022
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:10:22 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-stdlib for the canonical source repository
- * @copyright https://github.com/laminas/laminas-stdlib/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-stdlib/blob/master/LICENSE.md New BSD License
- */
-
 /*
  * Set error reporting to the level to which Laminas code must comply.
  */
