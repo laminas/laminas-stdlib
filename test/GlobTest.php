df40f463dace76a456e8111df41748dde256b52a
Gary Gitton <gary.gitton@gmail.com>
2021-12-15 00:12:43 +0100

    add test on glob and GLOB_BRACE flag
    Signed-off-by: Gary Gitton <gary.gitton@gmail.com>

--- test/GlobTest.php
+++ test/GlobTest.php
@@ -11,6 +11,7 @@ use PHPUnit\Framework\TestCase;
 use function count;
 use function defined;
 use function glob;
+use function realpath;
 use function str_repeat;
 
 use const GLOB_BRACE;
@@ -23,15 +24,29 @@ class GlobTest extends TestCase
             $this->markTestSkipped('GLOB_BRACE not available');
         }
 
-        self::assertEquals(
-            glob(__DIR__ . '/_files/{alph,bet}a', GLOB_BRACE),
-            Glob::glob(__DIR__ . '/_files/{alph,bet}a', Glob::GLOB_BRACE, true)
+        $expected = glob(__DIR__ . '/_files/{alph,bet}a', GLOB_BRACE);
+        $actual   = Glob::glob(
+            __DIR__ . '/_files/{alph,bet}a',
+            Glob::GLOB_BRACE,
+            true
+        );
+
+        self::assertEquals($actual, $expected);
+
+        $notExpectedPath = realpath(__DIR__ . '/_files/{alph,bet}a');
+
+        self::assertNotContains(
+            $notExpectedPath,
+            $actual
         );
     }
 
     public function testNonMatchingGlobReturnsArray(): void
     {
-        $result = Glob::glob('/some/path/{,*.}{this,orthis}.php', Glob::GLOB_BRACE);
+        $result = Glob::glob(
+            '/some/path/{,*.}{this,orthis}.php',
+            Glob::GLOB_BRACE
+        );
         self::assertIsArray($result);
     }
 
@@ -79,5 +94,53 @@ class GlobTest extends TestCase
                 ],
             ],
         ];
+    }
+
+    public function testGlobWithoutGlobBraceFlag(): void
+    {
+        $expected = [
+            realpath(__DIR__ . '/_files/{alph,bet}a'),
+        ];
+
+        self::assertEquals(
+            glob(__DIR__ . '/_files/{alph,bet}a', 0),
+            $expected
+        );
+    }
+
+    /**
+     * @psalm-return array<array-key, array{
+     *     int,
+     *     int,
+     *     bool
+     * }>
+     */
+    public function flagsIsEqualsToMethodDataProvider(): array
+    {
+        return [
+            [
+                Glob::GLOB_BRACE,
+                Glob::GLOB_BRACE,
+                true,
+            ],
+            [
+                Glob::GLOB_BRACE,
+                Glob::GLOB_NOSORT,
+                false,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider flagsIsEqualsToMethodDataProvider
+     */
+    public function testFlagsIsEqualsToMethod(
+        int $flags,
+        int $otherFlags,
+        bool $expected
+    ): void {
+        $actual = Glob::flagsIsEqualTo($flags, $otherFlags);
+
+        $this->assertEquals($expected, $actual);
     }
 }
